da1ae92f74ad15fd86364e223ee459a2370d8db8
Fernando Ituarte <fernando.ituarte@oclock.school>
2024-11-12 15:32:24 +0100

    update logo size

--- src/components/icons/LogoFI.jsx
+++ src/components/icons/LogoFI.jsx
@@ -4,10 +4,10 @@ export const LogoFI = () => {
       <svg
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
-        width="50"
+        width="40"
         zoomAndPan="magnify"
         viewBox="0 0 375 374.999991"
-        height="50"
+        height="40"
         preserveAspectRatio="xMidYMid meet"
         version="1.0"
       >
